06043bb9142aed510ffb944f4b6eb946b9bfb122
Joyanta Kumar <joyroy122004@gmail.com>
2025-05-15 21:48:00 +0600

    Deleted the visual technique

--- code/Joyanta-Kumar/Visual/modules/edge.py
+++
@@ -1,9 +0,0 @@
-from pygame import draw
-
-class Edge:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-    
-    def draw(self, window):
-        draw.line(window, "black", (self.start.x, self.start.y), (self.end.x, self.end.y), 2)
--- code/Joyanta-Kumar/Visual/modules/node.py
+++
@@ -1,9 +0,0 @@
-from pygame import draw
-
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def draw(self, window):
-        draw.circle(window, "black", (self.x, self.y), 10)
